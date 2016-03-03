18dc9429c695228633e269681dbbd5fb6af8879a
Martin Beeby <martinbeeby@hotmail.com>
2016-03-03 16:20:12 +0000

    Promise Order

--- whw.js
+++ whw.js
@@ -1,33 +1,14 @@
 'use strict';
-var first=document.createElement('H1');
-first.innerText = 'First';
+addElement("first")
+.then(x => {return addElement("second")})
+.then(x => { return addElement("third")})
+.then(x => { return addElement("fourth")})
 
-var second=document.createElement('H1');
-second.innerText = 'Second';
-
-var third=document.createElement('H1');
-third.innerText = 'Third'; 
-
-var fourth=document.createElement('H1');
-fourth.innerText = 'Fourth'; 
-
-// // document.body.appendChild(first);
-// // document.body.appendChild(second); 
-// doThisStuff()
-// async function doThisStuff(){
-//    await addElement(first);
-//    await addElement(second);
-//    await addElement(third);   
-// }
-
-addElement(first)
-.then(x => addElement(second))
-.then(x => addElement(third))
-.then(x => addElement(fourth))
-
-function addElement(element){
+function addElement(elementText){
     var myPromise = new Promise(function(resolve,reject){
     setTimeout(function(){
+        var element=document.createElement('H1');
+        element.innerText = `${elementText} ${Date.now()}`;
         document.body.appendChild(element);
         resolve();
     }, Math.random() * 2000);
