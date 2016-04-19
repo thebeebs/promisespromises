c158aa2b695bea1591a10a2e04a85a56cd7c9b4c
thebeebs <martinbeeby@hotmail.com>
2016-04-19 11:47:44 +0100

    Promise changes

--- whw.js
+++ whw.js
@@ -4,35 +4,22 @@ addElement("first")
 .then(x => { return addElement("third")})
 .then(x => { return addElement("fourth")})
 
-var second=document.createElement('H1');
-second.innerText = 'Second';
-
-var third=document.createElement('H1');
-third.innerText = 'Third'; 
-
-var fourth=document.createElement('H1');
-fourth.innerText = 'Fourth'; 
-
-// // document.body.appendChild(first);
-// // document.body.appendChild(second); 
-// doThisStuff()
-// async function doThisStuff(){
-//    await addElement(first);
-//    await addElement(second);
-//    await addElement(third);   
-// }
-
-addElement(first)
-.then(x => addElement(second))
-.then(x => addElement(third))
-.then(x => addElement(fourth))
-
-function addElement(element){
-    return new Promise(function(resolve,reject){
+function addElement(elementText){
+    var myPromise = new Promise(function(resolve,reject){
     setTimeout(function(){
         var element=document.createElement('H1');
         element.innerText = `${elementText} ${Date.now()}`;
         document.body.appendChild(element);
         resolve();
     }, Math.random() * 2000);
-})}
+});
+return myPromise;}
+// // document.body.appendChild(first);
+//
+ // document.body.appendChild(second); 
+// doThisStuff()
+// async function doThisStuff(){
+//    await addElement(first);
+//    await addElement(second);
+//    await addElement(third);   
+// }
